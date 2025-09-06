dc92674505490f8087e4401211093f4060998dfe
James Dimapilis <s2024100610@firstasia.edu.ph>
2025-09-06 17:23:42 +0700

    KOMET

--- EmployeeManagementSystem/Form1.Designer.cs
+++ EmployeeManagementSystem/Form1.Designer.cs
@@ -33,33 +33,43 @@
             this.btnMinimize = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
             this.btnMaximize = new System.Windows.Forms.Button();
+            this.btnMasterData = new System.Windows.Forms.Button();
+            this.btnAddEmp = new System.Windows.Forms.Button();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.pnlStage = new System.Windows.Forms.Panel();
+            this.pnlSideMenu.SuspendLayout();
             this.pnlTitleBar.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlSideMenu
             // 
+            this.pnlSideMenu.BackColor = System.Drawing.Color.DarkGray;
+            this.pnlSideMenu.Controls.Add(this.btnAddEmp);
+            this.pnlSideMenu.Controls.Add(this.btnMasterData);
             this.pnlSideMenu.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pnlSideMenu.Location = new System.Drawing.Point(0, 163);
+            this.pnlSideMenu.Location = new System.Drawing.Point(0, 84);
             this.pnlSideMenu.Name = "pnlSideMenu";
-            this.pnlSideMenu.Size = new System.Drawing.Size(220, 334);
+            this.pnlSideMenu.Size = new System.Drawing.Size(220, 413);
             this.pnlSideMenu.TabIndex = 0;
             // 
             // pnlTitleBar
             // 
             this.pnlTitleBar.AccessibleName = "";
+            this.pnlTitleBar.BackColor = System.Drawing.Color.DimGray;
             this.pnlTitleBar.Controls.Add(this.btnMinimize);
             this.pnlTitleBar.Controls.Add(this.btnClose);
             this.pnlTitleBar.Controls.Add(this.btnMaximize);
             this.pnlTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlTitleBar.Location = new System.Drawing.Point(0, 0);
             this.pnlTitleBar.Name = "pnlTitleBar";
-            this.pnlTitleBar.Size = new System.Drawing.Size(825, 163);
+            this.pnlTitleBar.Size = new System.Drawing.Size(825, 84);
             this.pnlTitleBar.TabIndex = 1;
+            this.pnlTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlTitleBar_MouseDown);
             // 
             // btnMinimize
             // 
             this.btnMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnMinimize.Location = new System.Drawing.Point(729, 125);
+            this.btnMinimize.Location = new System.Drawing.Point(729, 52);
             this.btnMinimize.Name = "btnMinimize";
             this.btnMinimize.Size = new System.Drawing.Size(24, 24);
             this.btnMinimize.TabIndex = 2;
@@ -70,17 +80,18 @@
             // btnClose
             // 
             this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnClose.Location = new System.Drawing.Point(789, 125);
+            this.btnClose.Location = new System.Drawing.Point(789, 52);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(24, 24);
             this.btnClose.TabIndex = 1;
             this.btnClose.Text = "✖️";
             this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // btnMaximize
             // 
             this.btnMaximize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnMaximize.Location = new System.Drawing.Point(759, 125);
+            this.btnMaximize.Location = new System.Drawing.Point(759, 52);
             this.btnMaximize.Name = "btnMaximize";
             this.btnMaximize.Size = new System.Drawing.Size(24, 24);
             this.btnMaximize.TabIndex = 0;
@@ -88,16 +99,52 @@
             this.btnMaximize.UseVisualStyleBackColor = true;
             this.btnMaximize.Click += new System.EventHandler(this.btnMaximize_Click);
             // 
+            // btnMasterData
+            // 
+            this.btnMasterData.BackColor = System.Drawing.Color.DarkGray;
+            this.btnMasterData.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMasterData.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMasterData.Font = new System.Drawing.Font("Microsoft Uighur", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMasterData.Location = new System.Drawing.Point(0, 0);
+            this.btnMasterData.Name = "btnMasterData";
+            this.btnMasterData.Size = new System.Drawing.Size(220, 45);
+            this.btnMasterData.TabIndex = 0;
+            this.btnMasterData.Text = "MASTER DATA";
+            this.btnMasterData.UseVisualStyleBackColor = false;
+            this.btnMasterData.Click += new System.EventHandler(this.btnMasterData_Click);
+            // 
+            // btnAddEmp
+            // 
+            this.btnAddEmp.BackColor = System.Drawing.Color.DarkGray;
+            this.btnAddEmp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddEmp.Font = new System.Drawing.Font("Microsoft Uighur", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddEmp.Location = new System.Drawing.Point(0, 45);
+            this.btnAddEmp.Name = "btnAddEmp";
+            this.btnAddEmp.Size = new System.Drawing.Size(220, 46);
+            this.btnAddEmp.TabIndex = 1;
+            this.btnAddEmp.Text = "ADD EMPLOYEE";
+            this.btnAddEmp.UseVisualStyleBackColor = false;
+            // 
+            // pnlStage
+            // 
+            this.pnlStage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlStage.Location = new System.Drawing.Point(220, 84);
+            this.pnlStage.Name = "pnlStage";
+            this.pnlStage.Size = new System.Drawing.Size(605, 413);
+            this.pnlStage.TabIndex = 2;
+            // 
             // frmMotherForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(825, 497);
+            this.Controls.Add(this.pnlStage);
             this.Controls.Add(this.pnlSideMenu);
             this.Controls.Add(this.pnlTitleBar);
             this.Name = "frmMotherForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.frmMotherForm_Load);
+            this.pnlSideMenu.ResumeLayout(false);
             this.pnlTitleBar.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -110,6 +157,10 @@
         private System.Windows.Forms.Button btnMinimize;
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.Button btnMaximize;
+        private System.Windows.Forms.Button btnMasterData;
+        private System.Windows.Forms.Button btnAddEmp;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.Panel pnlStage;
     }
 }
 
